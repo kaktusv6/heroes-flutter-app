59b28aa1fa794ee2a441ec97ab123ba31ac7ef0a
Vasiliy <nkf@feip.co>
2020-05-05 22:31:19 +1000

    Update widget form

--- heroes/lib/widgets/login_form.dart
+++ heroes/lib/widgets/login_form.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:heroes/presenters/login/interface.dart';
+import 'package:load/load.dart';
 
 class LoginForm extends StatefulWidget {
+  final ILoginPresenter presenter;
+
+  const LoginForm({Key key, @required this.presenter}) : super(key: key);
+
   @override
   State<StatefulWidget> createState() => _LoginFormState();
 }
@@ -11,38 +17,51 @@ class _LoginFormState extends State<LoginForm> {
 
   @override
   Widget build(BuildContext context) {
+    this.widget.presenter.checkLogin().then((isLogin) {
+      if (isLogin) {
+        Navigator.pushReplacementNamed(context, '/games');
+      }
+    });
+
     _onLoginPasswordPressed() {
-
+      this
+        .widget
+        .presenter
+        .login(_usernameController.text, _passwordController.text)
+        .then(
+          (isSuccess) {
+            if (isSuccess) {
+              Navigator.pushReplacementNamed(context, '/games');
+            }
+          }
+        );
     }
 
-    _onGoToRegistrationPage() {
+    _onNavigateToRegistrationPage() {}
 
-    }
-
-    return Form(
-      child: Column(
-        children: <Widget>[
-          TextFormField(
-            decoration: InputDecoration(labelText: 'Username'),
-            controller: _usernameController,
-          ),
-          TextFormField(
-            decoration: InputDecoration(labelText: 'Password'),
-            controller: _passwordController,
-            obscureText: true,
-          ),
-          RaisedButton(
-            onPressed: _onLoginPasswordPressed,
-            child: Text('Login'),
-          ),
-          RaisedButton(
-            onPressed: _onGoToRegistrationPage,
-            child: Text('Registration'),
-          ),
-          Container(
-            child: null,
-          ),
-        ],
+    return Center(
+      child: Form(
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Username'),
+              controller: _usernameController,
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Password'),
+              controller: _passwordController,
+              obscureText: true,
+            ),
+            RaisedButton(
+              onPressed: _onLoginPasswordPressed,
+              child: Text('Login'),
+            ),
+            RaisedButton(
+              onPressed: _onNavigateToRegistrationPage,
+              child: Text('Signup'),
+            ),
+          ],
+        ),
       ),
     );
   }
