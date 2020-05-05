6685a5ed68a057b6686bac856f07ed9bb4136607
Vasiliy <nkf@feip.co>
2020-05-05 22:29:29 +1000

    Add splash screen

--- heroes/lib/main.dart
+++ heroes/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flare_splash_screen/flare_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:heroes/repositories/user.dart';
 import 'package:heroes/presenters/login/login.dart';
 import 'package:heroes/repositories/games/games.dart';
 import 'package:heroes/repositories/games/interface.dart';
@@ -41,10 +40,16 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: SplashScreen.navigate(
+        name: 'assets/intro.flr',
+        next: (_) => LoginPage(
+          presenter: LoginPresenter(userRepository: userRepository),
+        ),
+        until: () => Future.delayed(Duration(seconds: 5)),
+        startAnimation: '1',
+      ),
       routes: {
-        '/': (context) => Splash(),
       },
-
     );
   }
 }
