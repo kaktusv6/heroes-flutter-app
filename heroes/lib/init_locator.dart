1207d5fbc3b693bbe953ec08a932716649601434
Vasiliy <nkf@feip.co>
2020-05-05 22:31:34 +1000

    Add storages to locator

--- heroes/lib/init_locator.dart
+++ heroes/lib/init_locator.dart
@@ -1,8 +1,15 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
+import 'package:heroes/storages/api_key.dart';
+import 'package:heroes/storages/storage_token.dart';
+
+import 'api/heroes_api.dart';
 
 GetIt locator = GetIt.I;
 
 void setupLocator() {
   locator.registerLazySingleton(() => FlutterSecureStorage());
+  locator.registerLazySingleton(() => TokenStorage());
+  locator.registerLazySingleton(() => ApiKeyStorage());
+  locator.registerLazySingleton(() => HeroesApiClient());
 }
