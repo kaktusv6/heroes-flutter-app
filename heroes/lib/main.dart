f57c8db4a9d2b4d39967addbbd437462b5d14a6a
Vasiliy <nkf@feip.co>
2020-05-05 22:27:59 +1000

    Change initial user repository

--- heroes/lib/main.dart
+++ heroes/lib/main.dart
@@ -1,32 +1,40 @@
+import 'package:flare_splash_screen/flare_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:heroes/pages/splash.dart';
 import 'package:heroes/repositories/user.dart';
+import 'package:heroes/presenters/login/login.dart';
+import 'package:heroes/repositories/users/interface.dart';
+import 'package:heroes/repositories/users/user.dart';
 
 import 'init_locator.dart';
 
 void main() async {
   await DotEnv().load('.env');
   setupLocator();
-  final userRepository = UserRepository();
+  final IUserRepository userRepository = UserRepository();
 
   runApp(
-    App(
-      userRepository: userRepository
+    LoadingProvider(
+        child: App(
+            userRepository: userRepository,
+        )
     ),
   );
 }
 
 class App extends StatelessWidget {
-  final UserRepository userRepository;
+  final IUserRepository userRepository;
 
-  App({Key key, @required this.userRepository}) : super(key: key);
+  App({
+    Key key,
+    @required this.userRepository,
+  })
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/',
       routes: {
         '/': (context) => Splash(),
       },
